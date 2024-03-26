cbea88d207f93e3956de58665214b0c11a03d4c0
KBalane <john.kenneth_balane@questronix.com.ph>
2024-03-26 14:12:23 +0800

    resolution changes

--- src/App.jsx
+++ src/App.jsx
@@ -23,15 +23,15 @@ function App() {
   )
   return (
     <>
-      <Carousel className="w-full flex justify-center max-w-full h-96 xl:h-auto">
+      <Carousel className="w-full flex justify-center max-w-full xl:h-auto">
         <CarouselContent>
           <CarouselItem key="0" >
           <div>
             <Card className="border-black">
               <CardContent className="p-0 bg-cover flex items-left justify-left flex max-w-full w-full">
                 <div className='relative'>
-                  <video src={vid} autoPlay loop muted className='xl:h-full object-cover h-96'></video>
-                  <div className='text-white py-3 px-12 absolute left-0 bottom-0 flex flex-col space-y-1 font-ebgaramond font-[500] w-full'>
+                  <video src={vid} autoPlay loop muted className='xl:h-full object-cover h-[48rem]'></video>
+                  <div className='text-white py-3 xl:px-12 px-3 absolute left-0 bottom-0 flex flex-col space-y-1 font-ebgaramond font-[500] w-full'>
                     <span className='text-2xs xl:text-3xl'>TECHNOLOGICAL UNIVERSITY<br/> OF THE PHILIPPINES - MANILA</span>
                     <span className='text-3xl xl:text-5xl'>FINE ARTS DEPARTMENT</span>
                   </div>
@@ -45,8 +45,8 @@ function App() {
             <Card className="border-black">
               <CardContent className="p-0 bg-cover flex items-left justify-left flex">
                 <div className='relative'>
-                  <img src={picture} className='xl:h-full object-cover h-96'></img>
-                  <div className='text-white py-3 px-12 absolute left-0 bottom-0 flex flex-col font-ebgaramond font-[500] w-full'>
+                  <img src={picture} className='xl:h-full object-cover h-[48rem]'></img>
+                  <div className='text-white py-3 xl:px-12 px-3 absolute left-0 bottom-0 flex flex-col font-ebgaramond font-[500] w-full'>
                     <span className='text-md xl:text-3xl'>FINE ARTS DEPARTMENT</span>
                     <span className='text-5xl xl:text-5xl'>PLATES</span>
                     <Button className="text-white bg-learnmore-btn rounded hover:bg-white w-1/2 xl:w-1/4 font-roboto">Learn More</Button>
@@ -62,8 +62,8 @@ function App() {
             <Card className="border-black">
               <CardContent className="p-0 bg-cover flex items-left justify-left flex">
                 <div className='relative'>
-                  <img src={eventimg} className='xl:h-full object-cover h-96'></img>
-                  <div className='text-white py-3 px-12 absolute left-0 bottom-0 flex flex-col font-ebgaramond font-[500] w-full'>
+                  <img src={eventimg} className='xl:h-full object-cover h-[48rem]'></img>
+                  <div className='text-white py-3 xl:px-12 px-3 absolute left-0 bottom-0 flex flex-col font-ebgaramond font-[500] w-full'>
                     <span className='text-md xl:text-3xl'>FINE ARTS DEPARTMENT</span>
                     <span className='text-5xl xl:text-5xl'>EVENTS</span>
                     <Button className="text-white bg-learnmore-btn rounded hover:bg-white w-1/2 xl:w-1/4 font-roboto">Learn More</Button>
@@ -78,8 +78,8 @@ function App() {
             <Card className="border-black">
               <CardContent className="p-0 bg-cover flex items-left justify-left flex">
                 <div className='relative'>
-                  <img src={exhibitimg} className='xl:h-full object-cover h-96' ></img>
-                  <div className='text-white py-3 px-12 absolute left-0 bottom-0 flex flex-col font-ebgaramond font-[500] w-full'>
+                  <img src={exhibitimg} className='xl:h-full object-cover h-[48rem]' ></img>
+                  <div className='text-white py-3 xl:px-12 px-3 absolute left-0 bottom-0 flex flex-col font-ebgaramond font-[500] w-full'>
                     <span className='text-md xl:text-3xl'>FINE ARTS DEPARTMENT</span>
                     <span className='text-5xl xl:text-5xl'>EXHIBIT</span>
                     <Button className="text-white bg-learnmore-btn rounded hover:bg-white w-1/2 xl:w-1/4 font-roboto">Learn More</Button>
@@ -94,8 +94,8 @@ function App() {
             <Card className="border-black">
               <CardContent className="p-0 bg-cover flex items-left justify-left flex">
                 <div className='relative'>
-                  <img src={alumimg} className='xl:h-full object-cover h-96 overflow-hidden'></img>
-                  <div className='text-white py-3 px-12 absolute left-0 bottom-0 flex flex-col font-ebgaramond font-[500] w-full'>
+                  <img src={alumimg} className='xl:h-full object-cover h-[48rem] transform'></img>
+                  <div className='text-white py-3 xl:px-12 px-3 absolute left-0 bottom-0 flex flex-col font-ebgaramond font-[500] w-full'>
                     <span className='text-md xl:text-3xl'>FINE ARTS DEPARTMENT</span>
                     <span className='text-5xl xl:text-5xl'>ALUMNI</span>
                     <Button className="text-white bg-learnmore-btn rounded hover:bg-white w-1/2 xl:w-1/4 font-roboto">Learn More</Button>
