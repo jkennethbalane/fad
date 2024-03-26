6ebca14628f348b505268583bdca2ce5a4e372cf
KBalane <john.kenneth_balane@questronix.com.ph>
2024-03-26 15:27:25 +0800

    implemented proper file structure

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import NavBar from '../components/ui/NavBar.jsx'
+import Footer from '../components/ui/Footer.jsx'
+
+export default function About(){
+    return(
+    <>
+        <NavBar />
+        <Footer />
+    </>
+)}
